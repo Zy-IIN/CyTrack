9c79eb80baee57a198a3f4315cf6758430457dc7
Zy-IIN <116667635+Zy-IIN@users.noreply.github.com>
2026-04-08 15:39:58 +0800

    Delete benchmark directory

--- benchmark/features/__init__.py
+++
@@ -1,1 +0,0 @@
-from .sequence_builder import make_windows, build_sequences, generate_dummy_data
--- benchmark/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from .baselines import train_xgb, train_lr
-from .sequence_models import GRUPredictor, SequenceToOneLSTM
-from .fusion_network import ModalityGate, MultimodalFusion, SequenceToOnePredictor
